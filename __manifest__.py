a000f0a07c2cff3c2a4f24b9fd0c3ac0ead1b8f9
Martin <33923562+martinbrusco@users.noreply.github.com>
2025-04-07 15:36:26 +0200

    Add files via upload
    fin jornada 7/4

--- __manifest__.py
+++ __manifest__.py
@@ -1,25 +1,10 @@
 {
     'name': 'Theme Ninja Quiz',
-    "version": "17.0.1.0",
-    'summary': 'Theme Ninja Quiz',
-    'description': "Tema Ninja para Quiz",
-    'category': 'Demo',
-    'author': 'Martin',
-    'images': ['static/description/icon.png'],
-    'website': 'https://www.google.com',
-    'license': 'AGPL-3',
-    'depends': ['base', 'web'],
-    'data': [
-        'views/quiz_dashboard.xml'
-    ],
+    'version': '1.0',
+    'summary': 'Quiz game',
+    'category': 'Tools',
+    'depends': ['base', 'web', 'website'],
+    'data': ['views/quiz_templates.xml'],
     'installable': True,
-    'auto_install': False,
     'application': True,
-    'assets': {
-        'web.assets_backend': [
-            'theme_ninja_quiz/static/src/components/quiz_dashboard.js',
-            'theme_ninja_quiz/static/src/components/quiz_dashboard.xml',
-        ],
-    }
-
 }
--- controllers/main.py
+++ controllers/main.py
@@ -1,6 +1,13 @@
 from odoo import http
+from odoo.http import request
 
-class SaludoController(http.Controller):
-    @http.route('/quiz/hola', type='http', auth='public', website=True)
-    def hola(self, **kw):
-        return "¡Hola desde el módulo quiz!"
+class QuizController(http.Controller):
+
+    @http.route('/quiz/play', type='http', auth='public', website=True)
+    def quiz_play(self, **kw):
+        return request.render('theme_ninja_quiz.quiz_play_template', {})
+
+    @http.route('/quiz/join', type='http', auth="public", methods=["POST"], website=True)
+    def quiz_join(self, **post):
+        pin = post.get('pin')
+        return request.render('theme_ninja_quiz.quiz_join_template', {'pin': pin})
