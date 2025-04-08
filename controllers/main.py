3cfaf4d8320f142ca687a2e6499b7b7e266d98d0
Martin <33923562+martinbrusco@users.noreply.github.com>
2025-04-08 14:39:13 +0200

    Add files via upload

--- controllers/main.py
+++ controllers/main.py
@@ -3,11 +3,11 @@ from odoo.http import request
 
 class QuizController(http.Controller):
 
-    @http.route('/quiz/play', type='http', auth='public', website=True)
+    @http.route('/play', type='http', auth='public', website=True)
     def quiz_play(self, **kw):
-        return request.render('theme_ninja_quiz.quiz_play_template', {})
+        return request.render('ninja_quiz.quiz_play_template', {})
 
-    @http.route('/quiz/join', type='http', auth="public", methods=["POST"], website=True)
+    @http.route('/join', type='http', auth="public", methods=["POST"], website=True)
     def quiz_join(self, **post):
         pin = post.get('pin')
-        return request.render('theme_ninja_quiz.quiz_join_template', {'pin': pin})
+        return request.render('ninja_quiz.quiz_join_template', {'pin': pin})
