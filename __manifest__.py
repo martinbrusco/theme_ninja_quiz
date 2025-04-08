fee305d1942e76df02b4bf64c23e5e12a5210d78
Martin <33923562+martinbrusco@users.noreply.github.com>
2025-04-08 14:44:15 +0200

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,18 @@
 {
-    'name': 'Theme Ninja Quiz',
+    'name': 'Quiz Game',
     'version': '1.0',
     'summary': 'Quiz game',
     'category': 'Tools',
+    'author': 'Martin',
     'depends': ['base', 'web', 'website'],
-    'data': ['views/quiz_templates.xml'],
+    'data': [
+        'views/quiz_templates.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'ninja_quiz/static/src/css/quiz_styles.css',
+        ],
+    },
     'installable': True,
     'application': True,
 }
