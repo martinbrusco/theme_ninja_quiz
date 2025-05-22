fee647187a366a1ab01ba3fd30b3fc0f2854c7c6
Martin <martin.brusco@gmail.com>
2025-05-22 15:22:40 +0200

    Fin de jonada 22/5/2025

--- static/src/js/snippets.js
+++ static/src/js/snippets.js
@@ -1,42 +1,11 @@
-/** @odoo-module **/
-
-import { mount } from "@odoo/owl";
-import { KahootSurveyRunner } from "./kahoot_survey_runner";
-import { useService } from "@web/core/utils/hooks";
-
-const { Component, onMounted } = owl;
-
-export class QuizPlaySnippet extends Component {
-    static template = "theme_ninja_quiz.s_quiz_play";
-
-    setup() {
-        this.rpc = useService("rpc");
-        onMounted(() => this.mountQuizComponent());
-    }
-
-    async mountQuizComponent() {
-        const placeholder = this.el.querySelector("#kahoot-survey-runner-placeholder");
-        if (placeholder) {
-            const surveyId = parseInt(placeholder.dataset.surveyId) || null;
-            const token = placeholder.dataset.token || null;
-            const surveyExists = placeholder.dataset.surveyExists === "true";
-
-            if (surveyId && token) {
-                mount(KahootSurveyRunner, placeholder, {
-                    props: {
-                        surveyId,
-                        token,
-                        surveyExists,
-                    },
-                });
-            }
-        }
-    }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const quizPlays = document.querySelectorAll(".s_quiz_play");
-    quizPlays.forEach((el) => {
-        mount(QuizPlaySnippet, el);
+odoo.define('@theme_ninja_quiz/js/snippets', ['web.public.widget'], function (require) {
+    'use strict';
+    var publicWidget = require('web.public.widget');
+    publicWidget.registry.SnippetsWidget = publicWidget.Widget.extend({
+        selector: '.s_quiz_home, .s_quiz_play',
+        start: function () {
+            console.log('Snippets widget initialized');
+        },
     });
+    return publicWidget.registry.SnippetsWidget;
 });
