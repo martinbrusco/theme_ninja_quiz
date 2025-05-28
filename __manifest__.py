22987e509edc497b56581f4d7a3a50ce2dc4e09a
Martin <martin.brusco@gmail.com>
2025-05-28 22:36:12 +0200

    cambio

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     "name": "Theme Ninja Quiz",
     "description": "Un tema que emula una página de ingreso de PIN para un quiz",
     "category": "Theme/Creative",
-    "version": "1.2", # Incrementa la versión por los cambios
+    "version": "1.2", 
     "author": "Martin Brusco",
     "license": "LGPL-3",
     "depends": ["website", "survey"],
@@ -10,21 +10,21 @@
         "data/config.xml",
         "views/layout.xml",
         "views/footer.xml",
-        "views/components.xml", # Si aún lo usas y está definido
+        "views/components.xml", 
         "views/survey_views.xml",
-        "views/snippets/s_quiz_home.xml",   # Define el snippet de inicio
-        "views/snippets/s_quiz_play.xml",   # Define el snippet de juego
-        "views/snippets/snippets.xml",      # Registra los snippets en el editor
-        "views/website_pages.xml",          # Define las páginas contenedoras
+        "views/snippets/s_quiz_home.xml",  
+        "views/snippets/s_quiz_play.xml",  
+        "views/snippets/snippets.xml",     
+        "views/website_pages.xml",      
     ],
     "assets": {
         "web.assets_frontend": [
             "theme_ninja_quiz/static/src/scss/custom.scss",
             # OWL component and related JS for KahootSurveyRunner
             "theme_ninja_quiz/static/src/js/lib/SurveyDataService.js",
-            "theme_ninja_quiz/static/src/js/lib/StateManager.js", # Si es necesario para KahootSurveyRunner
-            "theme_ninja_quiz/static/src/js/kahoot_survey_runner.js", # Tu componente OWL principal
-            "theme_ninja_quiz/static/src/js/lib/mountComponent.js", # El script que monta el componente
+            "theme_ninja_quiz/static/src/js/lib/StateManager.js", 
+            "theme_ninja_quiz/static/src/js/kahoot_survey_runner.js", 
+            "theme_ninja_quiz/static/src/js/lib/mountComponent.js", 
         ],
     },
     "application": False,
